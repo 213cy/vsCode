96ee2421ac7e529b389d70022e40b0a0620873ff
213cyblue <213cyy@gmail.com>
2021-06-30 18:18:34 +0800

    mature capture

--- Capturing war3/callbacks.c
+++ Capturing war3/callbacks.c
@@ -12,9 +12,10 @@
 #define image_width 256
 #define image_height 210
 
-// int mapx = 255, mapy = 210;
-int mapx = image_width, mapy = image_height;
-int mapx1, mapy1;
+// int cx = 255, cy = 210;
+int cx = image_width, cy = image_height;
+int cx1, cy1;
+
 
 HWND hWndScreen;
 HDC hdcScreen;
@@ -40,15 +41,15 @@ HWND initCapture()
     if (hWndScreen == NULL)
     {
         // hWndScreen = GetDesktopWindow();
-        mapx1 = 133, mapy1 = 581;
+        cx1 = 133, cy1 = 581;
     }
     else
-        mapx1 = 133, mapy1 = 681;
+        cx1 = 133, cy1 = 681;
 
     // Retrieve the device context handle to get the client area of war3 window.
     hdcScreen = GetWindowDC(hWndScreen); // hdcScreen = GetDC(hWndScreen);
     // Create a compatible bitmap from the Window DC.
-    hbmScreen = CreateCompatibleBitmap(hdcScreen, mapx, mapy);
+    hbmScreen = CreateCompatibleBitmap(hdcScreen, cx, cy);
     // Create a compatible DC, which is used in a BitBlt from the window DC.
     hdcMemDC = CreateCompatibleDC(hdcScreen);
     // Select the compatible bitmap into the compatible memory DC.
@@ -57,20 +58,20 @@ HWND initCapture()
     hPen = CreatePen(PS_SOLID, 1, RGB(0, 255, 0));
 
     bi.biSize = sizeof(BITMAPINFOHEADER);
-    bi.biWidth = mapx;
+    bi.biWidth = cx;
     //  If biHeight is negative, the bitmap is a top-down DIB
     // and its origin is the upper-left corner.
-    bi.biHeight = -mapy; // -mapy
+    bi.biHeight = -cy; // -cy
     bi.biPlanes = 1;
-    bi.biBitCount = 24; // try 24 and 3 * mapx * mapy;
+    bi.biBitCount = 24; // try 24 and 3 * cx * cy;
     bi.biCompression = BI_RGB;
 
     // GetDIBits remarks: "The scan lines must be aligned on a DWORD ..."
     // See https://docs.microsoft.com/en-us/windows/win32/api/wingdi/nf-wingdi-getdibits#remarks
-    // 即需要保证 (mapx * bi.biBitCount / 32) 为一个整数
-    // 当 bi.biBitCount=24 时,要求mapx能被4整除
-    // 综合考虑 令 mapx =256
-    dwBmpSize = 3 * mapx * mapy; // bi.biBitCount/8 =3
+    // 即需要保证 (cx * bi.biBitCount / 32) 为一个整数
+    // 当 bi.biBitCount=24 时,要求cx能被4整除
+    // 综合考虑 令 cx =256
+    dwBmpSize = 3 * cx * cy; // bi.biBitCount/8 =3
 
     hMapFile = CreateFileMapping(
         INVALID_HANDLE_VALUE, // use paging file
@@ -116,19 +117,19 @@ int CaptureAnImage(int parm1)
 {
 
     HGDIOBJ hPenOld = SelectObject(hdcScreen, hPen);
-    MoveToEx(hdcScreen, mapx1 - 1, mapy1 - 1, (LPPOINT)NULL);
-    LineTo(hdcScreen, mapx1 - 1, mapy1 + 1 + mapy);
-    LineTo(hdcScreen, mapx1 + 1 + mapx, mapy1 + 1 + mapy);
-    LineTo(hdcScreen, mapx1 + 1 + mapx, mapy1 - 1);
-    LineTo(hdcScreen, mapx1 - 1, mapy1 - 1);
+    MoveToEx(hdcScreen, cx1 - 1, cy1 - 1, (LPPOINT)NULL);
+    LineTo(hdcScreen, cx1 - 1, cy1 + 1 + cy);
+    LineTo(hdcScreen, cx1 + 1 + cx, cy1 + 1 + cy);
+    LineTo(hdcScreen, cx1 + 1 + cx, cy1 - 1);
+    LineTo(hdcScreen, cx1 - 1, cy1 - 1);
     SelectObject(hdcScreen, hPenOld);
 
     // Bit block transfer into our compatible memory DC.
     // so the compatible bitmap contain the region of war3 screen
-    BitBlt(hdcMemDC, 0, 0, mapx, mapy, hdcScreen, mapx1, mapy1, SRCCOPY);
+    BitBlt(hdcMemDC, 0, 0, cx, cy, hdcScreen, cx1, cy1, SRCCOPY);
     // Gets the "bits" from the bitmap, and copies them into a buffer
     // that's pointed to by lpwar3image.
-    int scanline = GetDIBits(hdcMemDC, hbmScreen, 0, (UINT)mapy,
+    int scanline = GetDIBits(hdcMemDC, hbmScreen, 0, (UINT)cy,
                              lpwar3image, (BITMAPINFO *)&bi, DIB_RGB_COLORS);
     printf("\n [getbit] Copyed (%d) lines !(%d) bytes ! (%d) bits per pixel !",
            scanline, bi.biSizeImage, bi.biBitCount);
@@ -161,10 +162,10 @@ void ImageSave()
 
     int i, j, k;
     for (k = 0; k < 3; k++)
-        for (j = 0; j < mapy; j++)
-            for (i = 0; i < mapx; i++)
+        for (j = 0; j < cy; j++)
+            for (i = 0; i < cx; i++)
             {
-                lpTmp[i * mapy + j + k * mapx * mapy] = lpwar3image[(j * mapx + i) * 3 + 2 - k];
+                lpTmp[i * cy + j + k * cx * cy] = lpwar3image[(j * cx + i) * 3 + 2 - k];
             }
     FILE *file;
     fopen_s(&file, "image_arranged.bin", "wb");
--- Capturing war3/capture.c
+++ Capturing war3/capture.c
@@ -12,21 +12,34 @@
 
 #define IDT_TIMER 0
 #define WAITFORCAPTUER_THREAD_MSG WM_USER + 100
+#define REINITIALIZE_THREAD_MSG WM_USER + 101
 
-WCHAR classname[50];
+DWORD TID;
 HWND hwindowObj;
-UINT_PTR timerID;
+WCHAR classname[50];
 
-VOID CALLBACK MyTimerProc(HWND hwnd, UINT message, UINT idTimer, DWORD dwTime)
+void startCapture()
 {
-    printf("\n = = = window(0x%X) timer time(%d) up!", hwnd, dwTime);
-    KillTimer(NULL, timerID);
-    printf("\n = = = KillTimer...");
+    hwindowObj = initCapture();
+    RealGetWindowClass(hwindowObj, classname, 50);
+
+    OpenClipboard(NULL);
+    EmptyClipboard();
+    HGLOBAL hMem = GlobalAlloc(GMEM_MOVEABLE, sizeof(TID) + sizeof(hwindowObj));
+    LPDWORD lpMem = (DWORD *)GlobalLock(hMem);
+    memcpy(lpMem, &TID, 4);
+    memcpy(lpMem + 1, &hwindowObj, 8);
+    // memcpy((char *)lpMem , "abcdabcd", 8);
+    GlobalUnlock(hMem);
+    HANDLE hdata = SetClipboardData(CF_TEXT, hMem);
+    printf("\n # # # Paste PID to clipboard(0x%x)... ", hdata);
+    CloseClipboard();
 }
 
 DWORD WINAPI newThread(LPVOID lpParam)
 {
-    timerID = SetTimer(NULL, IDT_TIMER, 10000, (TIMERPROC)MyTimerProc);
+    // timerID = SetTimer(NULL, IDT_TIMER, 10000, (TIMERPROC)MyTimerProc);
+    UINT_PTR timerID = SetTimer(NULL, IDT_TIMER, 10000, NULL);
     printf("\n = = = timer(%d) start!", timerID);
 
     RegisterHotKey(NULL, 1, MOD_ALT | MOD_CONTROL, 0x41); //0x41 is 'a'
@@ -40,7 +53,10 @@ DWORD WINAPI newThread(LPVOID lpParam)
         // printf("\n = = = msg.message %d ", msg.message);
         if (msg.message == WM_TIMER)
         {
-            DispatchMessage(&msg);
+            // DispatchMessage(&msg);
+            printf("\n = = = timer(%d) time(%d) up!", msg.wParam, msg.time);
+            KillTimer(NULL, timerID);
+            printf("\n = = = KillTimer...");
             continue;
         }
         if (msg.message == WM_HOTKEY)
@@ -53,17 +69,14 @@ DWORD WINAPI newThread(LPVOID lpParam)
                 ImageSave();
                 break;
             case 2:
-                printf("\n = = = WM_HOTKEY B received, Reinitiallizing...");
+                printf("\n = = = WM_HOTKEY B received, Reinitializing...");
                 endCapture();
-                hwindowObj = initCapture();
-                RealGetWindowClass(hwindowObj, classname, 50);
+                startCapture();
                 break;
             case 3:
                 printf("\n = = = WM_HOTKEY C received, perserved hotkey");
-                // WM_MOUSEMOVE  WM_LBUTTONDOWN  WM_LBUTTONUP
-                SendMessage(hwindowObj, WM_LBUTTONDOWN, 1, MAKELPARAM(100, 100));
-                SendMessage(hwindowObj, WM_LBUTTONUP, 1, MAKELPARAM(100, 100));
-
+                mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0); // 按下
+                mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);   //弹起
                 break;
             }
             continue;
@@ -72,6 +85,13 @@ DWORD WINAPI newThread(LPVOID lpParam)
         {
             printf("\n = = = WAITFORCAPTUER_THREAD_MSG message received");
             CaptureAnImage(0);
+            continue;
+        }
+        if (msg.message == REINITIALIZE_THREAD_MSG)
+        {
+            printf("\n = = = REINITIALIZE_THREAD_MSG message received");
+            endCapture();
+            startCapture();
             continue;
         }
     }
@@ -84,10 +104,6 @@ int main()
     SetConsoleOutputCP(65001);
     // MessageBox(NULL, L"No timer is available.", L"Failed", MB_OK);
 
-    hwindowObj = initCapture();
-    RealGetWindowClass(hwindowObj, classname, 50);
-
-    DWORD TID;
     HANDLE hThreadCmd = CreateThread(
         NULL,      // default security attributes
         0,         // use default stack size
@@ -98,18 +114,7 @@ int main()
     printf("\n # # # Create Thread(0x%x)...", hThreadCmd);
     CloseHandle(hThreadCmd);
 
-    OpenClipboard(NULL);
-    EmptyClipboard();
-    HGLOBAL hMem = GlobalAlloc(GMEM_MOVEABLE, 2 * 4);
-    LPDWORD lpMem = (DWORD *)GlobalLock(hMem);
-    memcpy(lpMem, &TID, 4);
-    int a = 12345;
-    memcpy(lpMem + 1, &a, 4);
-    // memcpy((char *)lpMem , "abcdabcd", 8);
-    GlobalUnlock(hMem);
-    HANDLE hdata = SetClipboardData(CF_TEXT, hMem);
-    printf("\n # # # Paste PID to clipboard(0x%x)... ", hdata);
-    CloseClipboard();
+    startCapture();
 
     char buff[80] = {"0"};
     char *codestr = buff + 1; //(char *) (buff+1)
@@ -123,9 +128,9 @@ int main()
         printf("\n 3 - Save image");
         printf("\n 4 - Mouse behaviour simulate");
         printf("\n 9 - Exit");
-        printf("\n HOTKEYS: CONTROL+ALT+a - Image capture and save");
-        printf("\n HOTKEYS: CONTROL+ALT+b - Reinitialize");
-        printf("\n HOTKEYS: CONTROL+ALT+c - Test");
+        printf("\n HOTKEYS: Ctrl+Alt+a - Image capture and save");
+        printf("\n HOTKEYS: Ctrl+Alt+b - Reinitialize");
+        printf("\n HOTKEYS: Ctrl+Alt+c - Test");
         printf("\n请输入操作码(opcode)：  ");
 
         scanf("%s", codestr);
@@ -138,8 +143,8 @@ int main()
             OpenClipboard(NULL);
             HGLOBAL hMem2 = GetClipboardData(CF_TEXT);
             LPDWORD lpMem2 = (DWORD *)GlobalLock(hMem2);
-            printf("\ndata to wirte:(%d) (%d)\ndata in clipboard:(%d) (%d)",
-                   TID, a, *lpMem2, *(lpMem2 + 1));
+            printf("\ndata to wirte:(%d) (0x%x)\ndata in clipboard:(%d) (0x%x)",
+                   TID, hwindowObj, *lpMem2, *(HANDLE *)(lpMem2 + 1));
             GlobalUnlock(hMem2);
             CloseClipboard();
             break;
@@ -157,6 +162,10 @@ int main()
             ImageSave();
             break;
         case 4:;
+            // WM_MOUSEMOVE  WM_LBUTTONDOWN  WM_LBUTTONUP
+            SendMessage(hwindowObj, WM_LBUTTONDOWN, 0, MAKELPARAM(100, 100));
+            Sleep(300);
+            SendMessage(hwindowObj, WM_LBUTTONUP, 0, MAKELPARAM(100, 100));
 
             INPUT inputs = {0};
             inputs.type = INPUT_MOUSE;
@@ -173,14 +182,12 @@ int main()
             m_InPut[0].mi.dwFlags = MOUSEEVENTF_LEFTDOWN;
             m_InPut[1].mi.dwFlags = MOUSEEVENTF_LEFTUP;
             SendInput(2, m_InPut, sizeof(INPUT));
-            Sleep(1000);
-
-            mouse_event(MOUSEEVENTF_ABSOLUTE | MOUSEEVENTF_MOVE, 500, 500, 0, 0);
 
             break;
-
         case 9:
             printf("\n # # # exit program.");
+            endCapture();
+
             break;
         default:
             printf("\n # # # can't resloved your entered opcode (%d)\"%s\".", opcode, codestr);
